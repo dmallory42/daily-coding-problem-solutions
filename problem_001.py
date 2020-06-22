c625034ac47b398f467d4ccb5008b4224896a14f
Daniel Mallory <d.mallory42@gmail.com>
2020-06-22 10:22:46 +0100

    Solution for Problem #001

--- problem_001.py
+++ problem_001.py
@@ -1,3 +1,14 @@
+# ---------------------------
+# Problem 1
+# Given a list of numbers and a number k, return whether
+# any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true
+# since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+# ---------------------------
+
 def check_sums(values: list, k: int):
     # Create a shallow copy of values:
     values_copy = [*values]
